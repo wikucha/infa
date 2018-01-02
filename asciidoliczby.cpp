4ef2560513da552a5568e267858dced150e2908b
wikucha <34515024+wikucha@users.noreply.github.com>
2018-01-02 11:52:39 +0100

    Add files via upload

---
+++ asciidoliczby.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+char cyfra;
+cyfra = '6';
+cout<<"cyfra= "<<cyfra<<endl;
+
+int liczba;
+liczba = cyfra-'0';
+cout<<"liczba= "<<liczba<<endl;
+
+
+liczba = 3;
+cyfra=liczba+48;
+cout<<"cyfra= "<<cyfra<<endl;
+    return 0;
+}
